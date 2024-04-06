88afacb6ed717456b1aaffa4d09269e02b987741
Sallyrideauto <riru16@knou.ac.kr>
2024-04-07 01:23:47 +0900

    download cpp ellens alien game

---
+++ cpp/ellens-alien-game/ellens_alien_game.cpp
@@ -0,0 +1,45 @@
+namespace targets
+{
+    // TODO: Insert the code for the alien class here
+    class Alien
+    {
+    public:
+        Alien(int new_x, int new_y)
+        {
+            x_coordinate = new_x;
+            y_coordinate = new_y;
+        }
+
+        int get_health()
+        {
+            return health;
+        }
+
+        bool hit()
+        {
+            health--;
+            return true;
+        }
+
+        bool is_alive()
+        {
+            return health > 0;
+        }
+
+        bool teleport(int new_x, int new_y)
+        {
+            x_coordinate = new_x;
+            y_coordinate = new_y;
+            return true;
+        }
+
+        bool collision_detection(Alien other)
+        {
+            return other.x_coordinate == x_coordinate && other.y_coordinate == y_coordinate;
+        }
+
+        int x_coordinate;
+        int y_coordinate;
+        int health{3};
+    };
+} // namespace targets
