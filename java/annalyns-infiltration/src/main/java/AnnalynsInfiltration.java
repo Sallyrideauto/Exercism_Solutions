de698e420d9830f684e68925e6679818bbe2e774
Sallyrideauto <riru16@knou.ac.kr>
2024-05-08 18:51:38 +0900

    annalyn's java 파일 논리식 구조 수정

--- java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
+++ java/annalyns-infiltration/src/main/java/AnnalynsInfiltration.java
@@ -1,22 +1,20 @@
 class AnnalynsInfiltration {
     public static boolean canFastAttack(boolean knightIsAwake) {
-        return !knightIsAwake;
+        return !knightIsAwake; // 기사가 깨어 있지 않을 경우 빠른 공격이 가능
     }
 
     public static boolean canSpy(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake) {
-        boolean nonAwakenKnight = canFastAttack(knightIsAwake);
-        return knightIsAwake || archerIsAwake || prisonerIsAwake;
+        return knightIsAwake || archerIsAwake || prisonerIsAwake; // 세 명 중 한 명이라도 깨어 있으면 스파이 활동 가능
     }
 
     public static boolean canSignalPrisoner(boolean archerIsAwake, boolean prisonerIsAwake) {
-        return !archerIsAwake && prisonerIsAwake;
+        return !archerIsAwake && prisonerIsAwake; // 궁수가 잠들어 있고 죄수가 깨어 있어야 죄수에게 신호 가능
     }
 
-    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake, boolean petDogIsPresent) {
-        if (petDogIsPresent) {
-            return !archerIsAwake;
-        } else {
-            return !knightIsAwake && !archerIsAwake && prisonerIsAwake;
-        }
+    public static boolean canFreePrisoner(boolean knightIsAwake, boolean archerIsAwake, boolean prisonerIsAwake,
+            boolean petDogIsPresent) {
+        // 죄수를 해방 가능한 조건은 애완견이 있을 때와 없을 때로 나뉨
+        return (petDogIsPresent && !archerIsAwake)
+                || (!petDogIsPresent && !knightIsAwake && !archerIsAwake && prisonerIsAwake);
     }
 }
