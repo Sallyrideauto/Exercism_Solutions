fc05637a2bf0fbd18dc12644a71abe516d9aea15
Sallyrideauto <riru16@knou.ac.kr>
2024-05-18 23:00:35 +0900

    download cpp raindrops

---
+++ cpp/raindrops/raindrops.h
@@ -0,0 +1,11 @@
+#if !defined(RAINDROPS_H)
+#define RAINDROPS_H
+
+#include <string> // string 타이핑을 위해 필요
+
+namespace raindrops
+{
+    std::string convert(int number); // convert 함수 선언
+} // namespace raindrops
+
+#endif // RAINDROPS_H
