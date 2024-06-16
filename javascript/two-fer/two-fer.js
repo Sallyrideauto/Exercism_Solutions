6f115d0f612a16dfa78928293cee7b0ce9cab71b
Sallyrideauto <riru16@knou.ac.kr>
2024-06-16 21:58:00 +0900

    download javascript two fer

---
+++ javascript/two-fer/two-fer.js
@@ -0,0 +1,6 @@
+//
+// This is only a SKELETON file for the 'Two fer' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const twoFer = (name = "you") => `One for ${name}, one for me.`; // ES6 템플릿 리터럴을 사용하여 문자열을 구성 및 반환
